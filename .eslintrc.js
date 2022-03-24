a0b815e617e81db260c2e7ba6374599abc96629c
Ana Wagner <ana.wagner@kochgs.com>
2022-03-24 17:02:25 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: [
+    'html',
+  ],
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
